a2b7bbe0fe2126dc3000ed10a03c9c1b95e0345c
Ankit <ankit.prajapati@mindinventory.com>
2022-04-04 17:43:49 +0530

    changed template

--- template/android/app/src/main/java/com/projectname/newarchitecture/components/MainComponentsRegistry.java
+++ template/android/app/src/main/java/com/projectname/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.mindinventoryboilerplate.newarchitecture.components;
+package com.projectname.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
