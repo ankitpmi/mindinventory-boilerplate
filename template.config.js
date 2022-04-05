f33423940d3ffcd34d4fa76c4bb21bebfd427b51
Ankit <ankit.prajapati@mindinventory.com>
2022-04-05 12:20:02 +0530

    added new template

--- template.config.js
+++ template.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     // Placeholder used to rename and replace in files
     // package.json, index.json, android/, ios/
-    placeholderName: "ProjectName",
+    placeholderName: "miboilerplate",
   
     // Directory with template
-    templateDir: "./template",
+    templateDir: "./MIBoilerplate",
   
     // Path to script, which will be executed after init
     // postInitScript: "./script.js"
