9f34865978027bc1bfa830ddb2c5f115f4403acd
Ankit <ankit.prajapati@mindinventory.com>
2022-04-05 12:35:07 +0530

    changed placeholder name

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Placeholder used to rename and replace in files
     // package.json, index.json, android/, ios/
-    placeholderName: "miboilerplate",
+    placeholderName: "MIBoilerplate",
   
     // Directory with template
     templateDir: "./MIBoilerplate",
